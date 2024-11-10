6ff93e2fe084c0289400c94a41c9ec7955e23b3a
Muhammad Tanveer <muhammadtanveer0135@gmail.com>
2024-11-10 21:00:37 +0500

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Footer from "./home/components/Footer";
+import Header from "./home/components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,8 +30,14 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      > 
+      
+      <Header/>
+      
         {children}
+       
+        <Footer></Footer>
+        
       </body>
     </html>
   );
